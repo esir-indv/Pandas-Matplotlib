cc4447c9516a801b44f9ac5fedff7732caada33b
esir-indv <847045180@qq.com>
2023-11-30 09:48:35 +0800

    项目结构调整

--- 001.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame({'ID': [1, 2, 3], 'name': ['Tim', 'Victor', 'Nick']})
-# 设置索引
-df = df.set_index('ID')
-df.to_excel('/Users/fengliang/Documents/workspace/output.xlsx')
-print(df)
--- 009读取多层索引DataFrame.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# 文件路径
-path = '/Users/fengliang/Documents/workspace/Pandas&Matplotlib/excel/2023年社保卡、代发工资任务进度表(10月31日).xlsx'
-# 读取Excel文件，并指定多层索引的列名，指定sheet
-df_excel = pd.read_excel(path, sheet_name='支行进度表', header=0, index_col=0)
-
-print(df_excel)
